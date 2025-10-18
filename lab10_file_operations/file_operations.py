fcf0e2ce5b40cbff5a96b69eac21e0f9ce1f8e8d
MAazhar07 <mohammed.azr07@gmail.com>
2025-10-18 02:38:01 +0000

    Lab 10 complete

---
+++ lab10_file_operations/file_operations.py
@@ -0,0 +1,42 @@
+"""
+Mohammed Azhar
+Oct 15, 2025
+Lab 9, file operation test
+"""
+
+def write_file(filename, msg):
+    with open(filename, "w") as file:
+        file.write(msg)
+
+def read_file(filename):
+    with open(filename, "r") as file:
+        return file.read()
+
+def append_file(filename, msg):
+    with open(filename, "a") as file:
+        file.write(msg)
+
+    #function from lab 7
+def email_read(filename, email):
+    try:
+        count_email = 0  
+        with open(filename, "r") as file1:
+            filelines = file1.readlines()  
+
+            # Loop through each line in the file
+            for eachline in filelines:
+                print(eachline.strip())
+                # Check if the email domain exists in the line (e.g. '@gmail', '@yahoo', etc.)
+                if email in eachline:
+                    count_email += 1  
+        # Return the final count
+        return count_email  
+
+    except FileNotFoundError:
+        # Exception if the file is not found
+        print(f"Error: The file {filename} was not found.")
+        return 0
+    except Exception as e:
+        # Handle any other exceptions that might occur
+        print(f"An error occurred: {e}")
+        return 0
